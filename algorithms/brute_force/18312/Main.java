55e66fcf5dc25921f70b0d7d1291829ded1b32d5
June <junejeong@yonsei.ac.kr>
2025-12-26 00:57:00 +0900

    18312

--- algorithms/brute_force/18312/Main.java
+++ algorithms/brute_force/18312/Main.java
@@ -14,64 +14,28 @@ public class Main {
         }
         return st.nextToken();
     }
-
-    static int occurs(int n, int k){
-        //  0-n
-        // n = 10a+b
-        int a = n/10;
-        int b = n%10;
-        // int ans = 0;
-        // if( a > k){ // 30 2
-        //     ans += 10;
-        //     ans += a-1;
-        //     if( b>= k) ans++;
-        // }
-        // else if ( a == k){ // 22 2
-        //     ans += b+1;
-        //     if(a == 0) {}
-        //     else ans += a;
-        // }
-        // else{ // a<k  19 9
-        //     ans += a;
-        //     if( b>= k) ans++;
-        // }
-        // return ans;
-        
-        // 십의자리가 K인 HH
-        int tens = 0;
-        int ones = 0;
-        if (a > k) 
-            tens = 10;
-        else if ( a==k) 
-            tens = b+1;
-        else 
-            tens =0;
-        // 일의자리가 K HH
-        ones = a;
-        if(b>=k) ones += 1;
-
-        if ( n>= 10*k+k) ones -=1; // 중복 빼주기
-
-        return tens + ones;
-    }
     public static void main(String argv[]) throws IOException{
        int N = Integer.parseInt(next());
-       int K = Integer.parseInt(next());
-        // 00 01 02 03 04 05 06 07 08 09 10 11 12 
-       int answer=0;
-       int hours = occurs(N, K);
+       String K = next();
+        
+       int count=0;
+        for( ; N>=0; N-- ) {
+            String s = String.valueOf(N);
+            if (N<10) s = "0" + s;
+        
+            for (int i = 0; i < 60; i++) {
+                    String minute = String.valueOf(i);
+                    if (i<10) minute = "0" + minute;
+                    for (int j = 0; j < 60; j++) {
+                        String sec = String.valueOf(j);
+                        if (j<10) sec = "0" + sec;
 
-        // System.out.println(hours);
-       answer = hours * 3600;
+                        if(s.contains(K) || minute.contains(K) || sec.contains(K)) count++;
+                    }
+            }
+        }   
+      
 
-       int minutes = occurs(59, K);
-       answer += (N+1-hours) * (minutes * 60);
-
-       int sec = minutes;
-
-       answer+= (N+1-hours) * (60-minutes) * sec ; 
-
-
-       System.out.println(answer);
+       System.out.println(count);
     }
 }
