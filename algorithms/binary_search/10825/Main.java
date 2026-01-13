1a97064a8978de82eadb1fe21e0009258dfefafd
June <junejeong@yonsei.ac.kr>
2026-01-13 21:15:39 +0900

    10825

--- algorithms/binary_search/10825/Main.java
+++ algorithms/binary_search/10825/Main.java
@@ -1,7 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,19 +23,38 @@ public class Main {
     }
     public static void main(String argv[]) throws IOException{
         int n = nextInt();
-        HashSet<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            set.add(nextInt());
+            list.add(nextInt());
         }
+        Collections.sort(list);
         n = nextInt();
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
-            if (set.contains(nextInt())){
-                System.out.println(1);
+            
+            int next = nextInt();
+            int l = 0, r=list.size();
+
+            while(l<r){
+                int m = l + (r-l) /2;
+                int result = list.get(m);
+                if (result<next){
+                    l=m +1;
+                }
+                else{
+                    r = m;
+                }
+            }
+
+            if( l < list.size() && list.get(l) == next){
+                sb.append(1).append('\n');
             }
             else{
-                System.out.println(0);
+                sb.append(0).append('\n');
             }
+            
         }
+        System.out.println(sb.toString());
         
     }
 }
